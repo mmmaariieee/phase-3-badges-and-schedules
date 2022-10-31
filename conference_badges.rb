b23db9242021241fa96db7149dbe98ee59f9e023
Mariia <m.goncharenkoo@icloud.com>
2022-10-31 00:00:36 -0400

    passed tests

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(array)
+    n=0
+    array.map do |name|
+        n += 1
+        "Hello, #{name}! You'll be assigned to room #{n}!"
+    end
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |badge|
+        puts badge
+    end
+    assign_rooms(array).each do |assignment|
+        puts assignment
+    end
+end
